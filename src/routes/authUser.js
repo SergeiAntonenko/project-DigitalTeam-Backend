74b32ef167e38dd57452bf6eb09373971d52066a
YouKnowWhoIs <leokatmax@gmail.com>
2024-07-21 14:07:50 +0300

    bidUp

--- src/routes/authUser.js
+++ src/routes/authUser.js
@@ -11,22 +11,22 @@ import { logoutUserController } from '../controllers/authUser.js';
 
 import { refreshUserSessionController } from '../controllers/authUser.js';
 
-const router = Router();
+const authRouter = Router();
 
-router.post(
+authRouter.post(
   '/register',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
 );
 
-router.post(
+authRouter.post(
   '/login',
   validateBody(loginUserSchema),
   ctrlWrapper(loginUserController),
 );
 
-router.post('/logout', ctrlWrapper(logoutUserController));
+authRouter.post('/logout', ctrlWrapper(logoutUserController));
 
-router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+authRouter.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
-export default router;
+export default authRouter;
