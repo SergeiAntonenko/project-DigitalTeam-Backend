281c6b706091bda1e2552a2cef5be683ab9ffeb8
Sergii Antonenko <gerbar50@gmail.com>
2024-07-25 16:34:40 +0300

    upd register user

--- src/controllers/authUser.js
+++ src/controllers/authUser.js
@@ -12,12 +12,15 @@ import { requestResetToken } from '../services/authUser.js';
 import { resetPassword } from '../services/authUser.js';
 
 export const registerUserController = async (req, res) => {
-  const user = await registerUser(req.body);
+  const { session, user } = await registerUser(req.body);
 
   res.json({
     status: 201,
     message: 'Successfully registered a user!',
-    data: user,
+    data: {
+      accessToken: session.accessToken,
+      user,
+    },
   });
 };
 
